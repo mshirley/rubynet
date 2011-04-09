aa144ffd24b8bb4ae50be9f87f1d014c5c10e7ea
mshirley <mshirley@gmail.com>
2011-04-09 15:27:28 -0700

    Comments

--- master.rb
+++ master.rb
@@ -1,9 +1,13 @@
 #!/usr/bin/ruby
-require 'socket'
-require 'zlib'
-require 'base64'
+require 'socket' 
+require 'zlib' # compression
+require 'base64' # not needed anymore but might be useful later
 inventory = []
+
+# Ocra is the ruby2exe app we're using.  this if loop prevents the compiler from executing it.
 if not defined?(Ocra)
+
+# This loop loads an inventory file, loads a masterkey and then loads slave node keys.  It will also create a blank dat file if it doesn't already exist
 if File.exist?("inventory.dat")
 	puts "inventory found, loading..."
 	inventorydat = File.open("inventory.dat", "r")
@@ -30,18 +34,28 @@ end
 
 server = TCPServer.new(2008)
 
+# This is where most of the action happens.  every new connection will create a new thread.  this will likely change significantly for persistent connections
 while (session = server.accept)
  Thread.start do
+# Any "puts" will go to the console for debug.  session.gets will receive a line of data from the connected client          
 	time = Time.new
 	puts time.strftime("%Y-%m-%d %H:%M:%S") + "-- connection from #{session.peeraddr[3]} opened"
-   	nodekey = session.gets
+   	# The client should send it's key as the first part of our communication
+        nodekey = session.gets
 	nodekey = nodekey.chomp
    	puts "nodekey #{nodekey}"
+        # Next we check our loaded inventory for the key that the node sent.  It should disconnect everything that isn't in the inventory.
 	if inventory.include?(nodekey)
+                # This is where all of our functions will run from.  The next sequence of events happens once per connection and then it closes.  
+                # session.puts send data to the client
 		session.puts "Welcome: #{nodekey}"
 		request = session.gets
 		request = request.chomp
 		session.puts "PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD-PAYLOAD"
+
+#  This is here as a template to create a session sync point to ensure the connection isn't messed up.  i'm sure we're reinventing the wheel
+#  EventMachine will be better for this whole thing.
+#
 #		response = session.gets
 #		response = response.chomp
 #		if response == "ok"
@@ -51,19 +65,20 @@ while (session = server.accept)
 #			session.close
 #		end
 		
-
+                # The node sends all of it's collected data and then post process
 		hostdata = []
 		while dataline = session.gets
 			hostdata << dataline
 		end
 		puts hostdata
-		decompString = Zlib::Inflate.inflate hostdata.to_s 
+                # The data comes across compressed for obvious reasons
+		decompString = Zlib::Inflate.inflate hostdata.to_s  # to_s converts the value of hostdata to a string
 		puts decompString	
 		File.open("nodedata.dat", "a") { |f| f.write(decompString) }
 		puts "connection from #{session.peeraddr[3]} closed"
-	else
+	else # any node not in the inventory get dropped
 		session.close
-	end   
-  end
-end
-end
+	end   # main client run and node verification
+  end #  thread loop
+end # server session loop
+end # ocra end
